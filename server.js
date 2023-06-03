e397aabeaabfc4938b0731a21aaeb0f97f9506de
Ahmed Wahba <5320002@pua.edu.eg>
2023-06-03 23:12:34 +0300

    remove calculator app

--- server.js
+++ server.js
@@ -2,21 +2,18 @@
 import express from 'express';
 const app = express();
 
-// define a route handler for the default home page
-app.use(express.static('public'));
-
-
-// Define your server's routes using app.get, app.post, etc.
+// This is the HTTP server and create a new instance of the http server object.
 app.get('/', (req, res) => {
-    res.sendFile("index.html");
-  });
+    res.send('Hello World!');
+});
 
-// Define a route to respond to a GET request for "about" page
+// This is the HTTP server and create a new instance of the http server object.
 app.get('/about', (req, res) => {
-    res.send('About page!');
-})
+    res.send('About Page!');
+});
+
 
 // Start your server by calling app.listen and passing in a port number.
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-})
+});
